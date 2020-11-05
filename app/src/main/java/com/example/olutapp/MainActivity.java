96699f2abb20ed15a093b985efd5b1971c2bc289
Gio <q.gio1984@gmail.com>
2020-11-05 21:27:50 +0200

    Initial commit - connected with the database Firebase

--- app/src/main/java/com/example/olutapp/MainActivity.java
+++ app/src/main/java/com/example/olutapp/MainActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        database = FirebaseDatabase.getInstance();
+        DatabaseReference ref = database.getReference("Beers");
+        //String key = ref.getKey();
+        //System.console().writer().println(ref.toString());
     }
 }

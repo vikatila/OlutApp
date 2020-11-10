cad283e0594e08111d710047ae37e1760cebd0dd
Gio <q.gio1984@gmail.com>
2020-11-10 18:45:59 +0200

    Added DatabaseHelper, AuthHelper, StorageHelper

---
+++ app/src/main/java/com/example/olutapp/AuthHelper.java
@@ -0,0 +1,4 @@
+package com.example.olutapp;
+
+public class AuthHelper {
+}
--- app/src/main/java/com/example/olutapp/MainActivity.java
+++ app/src/main/java/com/example/olutapp/MainActivity.java
@@ -3,21 +3,35 @@ package com.example.olutapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
-    FirebaseDatabase database;
+    private FirebaseAuth mAuth;
+    private DatabaseHelper dbHelper;
+    private StorageHelper storageHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        database = FirebaseDatabase.getInstance();
-        DatabaseReference ref = database.getReference("Beers");
+        dbHelper = new DatabaseHelper();
+        storageHelper = new StorageHelper();
+        dbHelper.Favorites(1);
         //String key = ref.getKey();
         //System.console().writer().println(ref.toString());
     }
+
+    public void onClickBtn(View view) {
+        List<String> list = dbHelper.getFavorites();
+        Log.d("1", "List");
+    }
+
 }
